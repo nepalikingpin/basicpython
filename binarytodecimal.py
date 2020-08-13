e1b5bddfa79fbdea05dde07d1cf1195d734a790e
Manish Bhattarai <bhaimanish00@gmail.com>
2020-08-13 22:54:17 +0545

    Added a second method

--- binarytodecimal.py
+++ binarytodecimal.py
@@ -1,9 +1,27 @@
 print('Binary to Decimal Conversion')
+print('Choose 1 for first method and 2 for second method')
+choice = int(input('Enter the method\t'))
+if (choice == 1):
 
-binaryinput = (input('Enter a binary number\t'))
-n = len(binaryinput)
-decimal = 0
-for x in binaryinput:
-    decimal = decimal + int(int(x)*(2**(n-1)))
-    n = n-1
-print('The decimal value is',decimal)
+# Method 1
+
+    binaryinput = (input('Enter a binary number\t'))
+    n = len(binaryinput)
+    decimal = 0
+    for x in binaryinput:
+        decimal = decimal + int(int(x)*(2**(n-1)))
+        n = n-1 
+    print('The decimal value is',decimal)
+
+else:
+
+# Method 2
+
+    binaryinput1 = int(input('Enter a binary number\t'))
+    decimal1, i = 0, 0
+    while (binaryinput1!=0):
+        n = binaryinput1 % 10
+        decimal1 = decimal1 + n * (2**i)
+        binaryinput1 = binaryinput1//10
+        i+=1
+    print('The decimal value is',decimal1)   
