ec81b02b400d5d7e9f938ab72d80b4257496e48e
Manish Bhattarai <bhaimanish00@gmail.com>
2020-08-15 09:35:46 +0545

    Added Prime or Composite

---
+++ primecomposite.py
@@ -0,0 +1,16 @@
+print("Check if a number is prime")
+#taking input from user
+number = int(input('Enter the number\t'))
+
+if number>1:
+
+    for i in range(2,number):
+        if(number%i) == 0:
+            print(number, "is composite")
+            print(i, "times",number//i,"is",number)
+            break
+    else:
+        print(number, 'is a prime number')
+
+else:
+    print(number,'Number is not a prime number')
