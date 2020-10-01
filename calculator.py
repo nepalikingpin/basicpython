dfadab0f95464d6e437ab4fbafd6a0f5889a4821
Mahesh C. Regmi <geekyarthurs@gmail.com>
2020-10-01 11:06:52 +0545

    documented and added cartesianproduct

--- calculator.py
+++ calculator.py
@@ -1,19 +1,19 @@
-#calculator
-#number input
-num1 = float(input('Enter the first number\t'))
-num2 = float(input('Enter the second number\t'))
+# calculator
+# number input
+num1: float = float(input('Enter the first number\t'))
+num2: float = float(input('Enter the second number\t'))
 print("'Sum', 'Difference', 'Multiplication', 'Division'")
 choice = input('Enter the calculation of your choice\t')
 if (choice == 'Sum'):
     result = num1 + num2
 elif (choice == 'Difference'):
-    result = num1- num2
+    result = num1 - num2
 elif (choice == 'Multiplication'):
     result = num1 * num2
 else:
     result = num1 / num2
-#print result
-if(result%2 == 0):
-    print('The result is ',int(result))
+# print result
+if(result % 2 == 0):
+    print('The result is ', int(result))
 else:
     print('The result is', result)
